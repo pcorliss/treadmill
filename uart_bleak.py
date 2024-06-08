9d7916552fcdc289dce2feea1dcf9ef860b1b681
Philip Corliss <pcorliss@50projects.com>
2024-06-08 17:46:31 -0500

    Working speed set and decoding

--- uart_bleak.py
+++ uart_bleak.py
@@ -2,6 +2,8 @@ import asyncio
 from bleak import BleakClient
 from bleak.backends.characteristic import BleakGATTCharacteristic
 import os
+import queue
+
 
 DEVICE_ADDRESS = '968C8C0A-BFD0-EBCD-E5E0-A0CB16F0BFE9'  # Replace with your device's address
 # Characteristic: 0000fff1-0000-1000-8000-00805f9b34fb (81)
@@ -25,14 +27,14 @@ INIT_SEQUENCE = [
 CMDS = {
     "start":    b"\xE1\x00\x00\x00\x00",
     "stop":     b"\xE0\x00\x00\x00\x00",
+    "speed":    b"\xD0\x00\x00\x00\x00",
 }
 
-#   def speed=(new_speed)
-#     new_speed = (new_speed * 100.0).round
-#     units = new_speed / 100
-#     hundredths = new_speed % 100
-#     send_command([0xd0, units, hundredths,0, 0].pack('C*'))
-#   end
+# Set speed 0.4 -> 3.0 MPH
+def speed_cmd(speed):
+    units = int(speed)
+    hundredths = int(speed * 100) % 100
+    return b"\xD0" + units.to_bytes(1, 'little') + hundredths.to_bytes(1, 'little') + b"\x00\x00"
 
 INFO_QUERIES = {
     # "steps":    b"\xA1\x88\x00\x00\x00",
@@ -42,9 +44,40 @@ INFO_QUERIES = {
     "speed":    b"\xA1\x82\x00\x00\x00"
 }
 
+q = queue.Queue()
+
 def handle_rx(_: BleakGATTCharacteristic, data: bytearray):
-    print("received:", data)
-    print("alt: ", list(data[2:5]))
+    data_name = q.get()
+    if data_name == "speed":
+        print("Speed:", decode_speed(data))
+    elif data_name == "distance":
+        print("Distance:", decode_distance(data))
+    elif data_name == "time":
+        t = decode_time(data)
+        h, m, s = t
+        print(f"Time: {h:d}:{m:02d}:{s:02d}")
+    elif data_name == "init":
+        pass
+    else:
+        print("received:", data_name, data, list(data))
+
+
+# Speed in MPH
+def decode_speed(data):
+    return data[2] + (data[3] / 100)
+
+# Distance in Miles
+def decode_distance(data):
+    return data[2] + (data[3] / 100)
+
+# Time in HH:MM:SS
+def decode_time(data):
+    return list(data[2:5])
+
+async def send_cmd(client, cmd, name):
+    q.put(name)
+    await client.write_gatt_char(CHARACTERISTIC_UUID, cmd, response=False)
+    await asyncio.sleep(5)
 
 async def run(address):
     async with BleakClient(address) as client:
@@ -52,23 +85,32 @@ async def run(address):
         for command in INIT_SEQUENCE:
             await client.write_gatt_char(CHARACTERISTIC_UUID, command)
 
+        # Receive notifications
         await client.start_notify(CHARACTERISTIC_UUID, handle_rx)
 
+        # Works!!!
+        # # Start the device
+        q.put('start')
+        await client.write_gatt_char(CHARACTERISTIC_UUID, CMDS["start"])
+        await asyncio.sleep(6)
+        # await send_cmd(client, CMDS["start"], "start")
+
+        q.put('set_speed')
+        print("Setting speed to 2.0", speed_cmd(2.0), list(speed_cmd(2.0)))
+        await client.write_gatt_char(CHARACTERISTIC_UUID, speed_cmd(2.0))
+        await asyncio.sleep(6)
 
         # Query information
-        for name, query in INFO_QUERIES.items():
-            print("sending:", name)
-            await client.write_gatt_char(CHARACTERISTIC_UUID, query, response=False)
-            await asyncio.sleep(3)
-            # response = await client.read_gatt_char(CHARACTERISTIC_UUID)
-            # print(f"{name}: {response}")
-            # response = await client.read_gatt_descriptor(86)
-            # print(f"{name}: {response}")
+        while True:
+            for name, query in INFO_QUERIES.items():
+                q.put(name)
+                await client.write_gatt_char(CHARACTERISTIC_UUID, query, response=False)
+                await asyncio.sleep(5)
 
         # Works!!!
         # # Start the device
         # await client.write_gatt_char(CHARACTERISTIC_UUID, CMDS["start"])
-        # await asyncio.sleep(10)
+        # await asyncio.sleep(5)
         # # Stop the device
         # await client.write_gatt_char(CHARACTERISTIC_UUID, CMDS["stop"])
 
